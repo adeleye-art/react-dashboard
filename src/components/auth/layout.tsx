276fedcaf1f8b9c361e54166f635e22f948c67a7
Adeleye Alarape <adalarape@wragbysolutions.com>
2025-02-27 20:58:50 +0100

    changes

--- src/components/auth/layout.tsx
+++ src/components/auth/layout.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import RouterLink from 'next/link';
+// import RouterLink from 'next/link';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
-import { paths } from '@/paths';
-import { DynamicLogo } from '@/components/core/logo';
+// import { paths } from '@/paths';
+// import { DynamicLogo } from '@/components/core/logo';
 
 export interface LayoutProps {
   children: React.ReactNode;
@@ -23,9 +23,9 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
     >
       <Box sx={{ display: 'flex', flex: '1 1 auto', flexDirection: 'column' }}>
         <Box sx={{ p: 3 }}>
-          <Box component={RouterLink} href={paths.home} sx={{ display: 'inline-block', fontSize: 0 }}>
+          {/* <Box component={RouterLink} href={paths.home} sx={{ display: 'inline-block', fontSize: 0 }}>
             <DynamicLogo colorDark="light" colorLight="dark" height={32} width={122} />
-          </Box>
+          </Box> */}
         </Box>
         <Box sx={{ alignItems: 'center', display: 'flex', flex: '1 1 auto', justifyContent: 'center', p: 3 }}>
           <Box sx={{ maxWidth: '450px', width: '100%' }}>{children}</Box>
@@ -46,11 +46,11 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
             <Typography color="inherit" sx={{ fontSize: '24px', lineHeight: '32px', textAlign: 'center' }} variant="h1">
               Welcome to{' '}
               <Box component="span" sx={{ color: '#15b79e' }}>
-                Devias Kit
+                remlap
               </Box>
             </Typography>
             <Typography align="center" variant="subtitle1">
-              A professional template that comes with ready-to-use MUI components.
+              A professional template .
             </Typography>
           </Stack>
           <Box sx={{ display: 'flex', justifyContent: 'center' }}>
